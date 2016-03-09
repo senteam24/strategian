31c760c3c2682f0646c1bd21f4ea034d48484d9b
TapanGujjar <gujjartapan@gmail.com>
2016-03-09 05:55:23 +0530

    added

--- wsgi/myproject/sen_team24/views.py
+++ wsgi/myproject/sen_team24/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	name="star";
-	html="<htm><body><h1>Strategian</h1></body><html>"
+	html="<htm><body><h1>Just Checking/h1></body><html>"
 	return HttpResponse(html);
